7c00f6275c692a636e7b8db46ce4e46f434917c9
Ivan <grabovivan@gmailcom>
2022-10-19 13:40:12 +0300

    num3 added

---
+++ num3/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+
+[3 7 22 2 78] -> 76 */
+
+int[] array = new int[10];
+fillArray(array);
+printArray(array);
+
+Console.WriteLine(difference(array));
+
+int[] fillArray(int[] arr)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+}
+
+void printArray(int[] arr)
+{
+    Console.WriteLine(string.Join(", ", arr));
+}
+
+int difference(int[] arr)
+{
+    int min = 1000000;
+    int max = 1;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] < min)
+        {
+            min = arr[i];
+        }
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max - min;
+} 
